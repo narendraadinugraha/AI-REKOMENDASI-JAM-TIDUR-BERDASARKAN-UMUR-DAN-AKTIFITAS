01be3afb2246ba93a81c0ecf0176288c2f9325dc
narendaadinugraha <narendra.23076@mhs.unesa.ac.id>
2025-06-01 23:06:54 +0700

    U

--- data_rules.py
+++ data_rules.py
@@ -1,22 +1,19 @@
-from data_rules import rekomendasi_tidur
-
-print("=== Rekomendasi Waktu Tidur Ideal ===")
-umur = int(input("Masukkan umur Anda: "))
-print("Pilih aktivitas Anda:")
-print("1. Pelajar\n2. Pekerja\n3. Atlet\n4. Santai")
-aktivitas_input = input("Masukkan nomor aktivitas (1-4): ")
-
-aktivitas_dict = {
-    "1": "pelajar",
-    "2": "pekerja",
-    "3": "atlet",
-    "4": "santai"
-}
-
-aktivitas = aktivitas_dict.get(aktivitas_input)
-
-if aktivitas:
-    durasi_tidur = rekomendasi_tidur(umur, aktivitas)
-    print(f"\n>> Rekomendasi waktu tidur Anda: {durasi_tidur} jam per malam")
-else:
-    print("Input aktivitas tidak valid.")
+def rekomendasi_tidur(umur, aktivitas):
+    if aktivitas == "pelajar":
+        if umur <= 12:
+            return 10
+        elif umur <= 18:
+            return 9
+        else:
+            return 8
+    elif aktivitas == "pekerja":
+        if umur < 40:
+            return 7.5
+        else:
+            return 7
+    elif aktivitas == "atlet":
+        return 9
+    elif aktivitas == "santai":
+        return 8
+    else:
+        return 7  # Default
